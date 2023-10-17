55267f886aae4520524b43b2ee457d9389b5fef6
Herculano De Biasi <herculano.debiasi@gmail.com>
2023-10-16 22:05:31 -0300

    Commit inicial!

---
+++ c++/obf_e.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#define e using
+#define ee namespace
+#define eee std
+#define eeee ;
+#define eeeee int
+#define eeeeee main
+#define eeeeeee (
+#define eeeeeeee )
+#define eeeeeeeee {
+#define eeeeeeeeee while
+#define eeeeeeeeeee true
+#define eeeeeeeeeeee cout
+#define eeeeeeeeeeeee <<
+#define eeeeeeeeeeeeee 'e'
+#define eeeeeeeeeeeeeee }
+	
+e
+ee
+eee
+eeee
+eeeee
+eeeeee
+eeeeeee
+eeeeeeee
+eeeeeeeee
+eeeeeeeeee
+eeeeeee
+eeeeeeeeeee
+eeeeeeee
+eeeeeeeeeeee
+eeeeeeeeeeeee
+eeeeeeeeeeeeee
+eeee
+eeeeeeeeeeeeeee
---
+++ polyquine/poly.c
@@ -0,0 +1,11 @@
+#include/*
+s='''*/<stdio.h>
+main(){char*_;/*==;sub _:lvalue{$_}<<s;#';<<s#'''
+from sys import* ; p = lambda x:stdout.write(x)
+def printf(a,*b):p (a%b)
+s
+#*/
+_=" #include/*%cs='''*/<stdio.h>%cmain(){char*_;/*==;sub _:lvalue{\%c_}<<s;#';<<s#'''%cfrom sys import* ; p = lambda x:stdout.write(x)%cdef printf(a,*b):p (a%%b)%cs%c#*/%c_=%c%s%c;printf(_,10,10,36,10,10,10,10,10,34,_,34,10,10,10,10);%c#/*%cs='''*/%c}//'''#==%c";printf(_,10,10,36,10,10,10,10,10,34,_,34,10,10,10,10);
+#/*
+s='''*/
+}//'''#==
